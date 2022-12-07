7bf9bd3ee96a440383a44e412ae1f0add741746c
ahualoh <alex_allie_h@yahoo.com>
2022-12-07 11:40:44 -0800

    Deliverable 2 compelted

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -30,11 +30,33 @@ ORDER BY emp_no, to_date DESC
 
 -- Step 16: Retrieve the number of employees by their most recent job title who are about to retire.
 SELECT COUNT(title), title
-INTO Retiring Titles
+INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC
 ;
 
 -- DELIVERABLE 2
+SELECT emp_no, first_name, last_name
+FROM employees
+;
 
+SELECT from_date, to_date 
+FROM dept_emp
+;
+
+SELECT title
+FROM titles
+;
+
+SELECT DISTINCT ON (emp_no) e.emp_no, e.first_name, e.last_name,
+	de.from_date, de.to_date,
+	t.title
+-- INTO mentorship_eligibility
+FROM employees e
+JOIN dept_emp de ON de.emp_no = e.emp_no
+JOIN titles t ON t.emp_no = e.emp_no
+WHERE de.to_date = '9999-01-01'
+	AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no ASC
+;
